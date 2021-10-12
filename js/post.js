dd68b121517af9d9d3b33227a0948f9d2842041a
anis <shahidsarker95@hotmail.com>
2021-10-12 18:32:55 +0600

    blog_demo: blogpost listing added

---
+++ js/post.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".post-body").each((index) => {
+    const postBody = $("#post-body" + index).text();
+    const maxLength = 400;
+    if (postBody.length > maxLength) {
+      const keep = postBody.substring(0, maxLength).split(" ").slice(0, -1).join(" ") + "...";
+      $("#post-body" + index).text(keep)
+    }
+  })
+})
